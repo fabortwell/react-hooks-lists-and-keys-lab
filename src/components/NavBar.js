08552f5b41d4b395d138aa502d0a184e69b5be54
Brian Sitati <briansit93@gmail.com>
2022-05-06 21:11:15 +0300

    updated projectItems.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,10 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  const tags = links.map((value)) => {
+  const tags = links.map((value) => {
     return <a key={value} href={"#"+value}>{value}</a>
   })
-console.log(tags)
+  console.log(tags)
 
   return <nav>{tags}</nav>;
 }
